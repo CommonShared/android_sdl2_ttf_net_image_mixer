db720d8f33e68d480598cd05997b33b0193c292d
ppshuai <ppshuai@MINIK91742.MINIK91742>
2019-10-12 14:06:05 +0800

    提交android版本SDL库及SDL2_image SDL2_net SDL2_mixer SDL2_ttf的动态库编译工程

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # android_sdl2_ttf_net_image_mixer
 Android 版本编译SDL2 SDL2_image SDL2_ttf SDL2_net SDL2_mixer动态库工程，可使用AndroidStudio或IntellijDEA进行编译
+
+创建Android工程步骤：
+File->Project->Android Empty Project,操作后再右键【工程名称】选择Link c++ native support->ndk-build,完成！！！
---
+++ sdl2_ttf_net_image_mixer/app/src/main/java/com/libs/sdl2_ttf_net_image_mixer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.libs.sdl2_ttf_net_image_mixer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
